64aff82ba49fbab311fb5873c58d16e65951fe22
xionghuifei <86391214+whndndn@users.noreply.github.com>
2024-05-01 11:32:08 +0800

    Update main.go

--- main.go
+++ main.go
@@ -225,7 +225,7 @@ func init() {
 	// 直接写死 AccessToken 时，请更改下面第二个参数
 	token := flag.String("t", "", "Set AccessToken of WSClient.")
 	// 直接写死 URL 时，请更改下面第二个参数
-	url := flag.String("u", "ws://192.168.2.21:3674", "Set Url of WSClient.")
+	url := flag.String("u", "ws://127.0.0.1:3001", "Set Url of WSClient.")
 	// 默认昵称
 	adana := flag.String("n", "熊会飞", "Set default nickname.")
 	prefix := flag.String("p", "/", "Set command prefix.")
